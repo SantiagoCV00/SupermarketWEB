69008559941e140d205453189f292ad15e937781
Santiago Carvajal Valencia <crj5850@gmail.com>
2025-05-15 23:29:15 -0500

    Backup_Proyecto

---
+++ SupermarketWEB/Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketWEB.Models
+{
+    public class Products
+    {
+        // [Key] -> Anotación si la propiedad no se llama Id, ejemplo ProductId
+        public int Id { get; set; } // Será la llave primaria
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; } // Será la llave foranea
+        public Category Category { get; set; } // Propiedad de navegación
+    }
+}
